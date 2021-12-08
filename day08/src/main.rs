80f4139fe5e22a90a05915dbac939914fbc621d7
Stijn Hoop <stijn@sandcat.nl>
2021-12-08 12:48:12 +0100

    Day 8 star 1

---
+++ day08/src/main.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::vec::Vec;
+
+fn star_one(lines: &Vec<String>) -> usize {
+    let mut count = 0;
+    for line in lines {
+        let mut split = line.split(" | ");
+        let _ = split.next().unwrap();
+        let currentdisplay = split.next().unwrap();
+
+        for digit in currentdisplay.split(' ') {
+            match digit.len() {
+                2 => count += 1,    // digit 1
+                4 => count += 1,    // digit 4
+                3 => count += 1,    // digit 7
+                7 => count += 1,    // digit 8
+                _ => (),
+            }
+        }
+    }
+
+    count as usize
+}
+
+fn main() {
+    let file = File::open("./input").expect("Unreadable input file ./input");
+    let lines: Vec<String> = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Could not read line"))
+        .collect();
+
+    let ans = star_one(&lines);
+    println!("Star one: {}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+    static TEST_DATA: &str = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+
+    #[test]
+    fn test_star_one() {
+        let lines: Vec<String> = TEST_DATA
+            .lines()
+            .map(|x| x.to_string())
+            .collect();
+
+        let ans = super::star_one(&lines);
+        assert_eq!(ans, 26);
+    }
+}
