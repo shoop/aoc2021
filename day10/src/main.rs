d74631e808490076de081582255a7d998bd2a9af
Stijn Hoop <stijn@sandcat.nl>
2021-12-10 09:09:01 +0100

    Day 10 star 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,7 +5,7 @@ use std::vec::Vec;
 #[derive(Debug)]
 enum ParseResult {
     Illegal(char),
-    Incomplete,
+    Incomplete(Vec<char>),
     Ok,
 }
 
@@ -55,7 +55,7 @@ fn parse_line(line: &str) -> ParseResult {
     }
 
     if parsestack.len() > 0 {
-        return ParseResult::Incomplete;
+        return ParseResult::Incomplete(parsestack.clone());
     }
 
     ParseResult::Ok
@@ -72,12 +72,36 @@ fn star_one(lines: &Vec<String>) -> usize {
                 '>' => total += 25137,
                 _ => panic!("Character {} not scored", ch)
             },
-            ParseResult::Incomplete => {},
+            ParseResult::Incomplete(_) => {},
             ParseResult::Ok => {},
         }
     }
 
     total
+}
+
+fn star_two(lines: &Vec<String>) -> usize {
+    let mut scores: Vec<usize> = Vec::new();
+    for line in lines {
+        match parse_line(line) {
+            ParseResult::Illegal(_) => {},
+            ParseResult::Incomplete(chars) => {
+                scores.push(chars.iter().rev().fold(0, |acc, ch| {
+                    acc * 5 + match ch {
+                        '(' => 1,
+                        '[' => 2,
+                        '{' => 3,
+                        '<' => 4,
+                        _ => panic!("Invalid character {} on stack", ch),
+                    }
+                }));
+            },
+            ParseResult::Ok => panic!("OK line found, not expected"),
+        }
+    }
+
+    scores.sort();
+    scores[scores.len() / 2]
 }
 
 fn main() {
@@ -89,6 +113,9 @@ fn main() {
 
     let ans = star_one(&lines);
     println!("Star one: {}", ans);
+
+    let ans = star_two(&lines);
+    println!("Star two: {}", ans);
 }
 
 #[cfg(test)]
@@ -111,4 +138,12 @@ mod tests {
         let ans = super::star_one(&lines);
         assert_eq!(ans, 26397);
     }
+
+    #[test]
+    fn test_star_two() {
+        let lines: Vec<String> = TEST_DATA.lines().map(|x| x.to_string()).collect();
+
+        let ans = super::star_two(&lines);
+        assert_eq!(ans, 288957);
+    }    
 }
