c5bd88063af118e26b56e65f9d063655070e8b91
Stijn Hoop <stijn@sandcat.nl>
2021-12-15 23:01:35 +0100

    Day 15 star 2 WIP
    Not sure what is wrong :-(

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -59,6 +59,77 @@ fn astar(map: &Vec<Vec<usize>>) -> Vec<(usize, usize)> {
     panic!("no path found")
 }
 
+fn astartwo(map: &Vec<Vec<usize>>) -> Vec<(usize, usize)> {
+    let mut result: Vec<(usize, usize)> = Vec::new();
+
+    let maxmapy = map.len();
+    let maxmapx = map[0].len();
+
+    let maxy = maxmapy * 5;
+    let maxx = maxmapx * 5;
+
+    let mut open_set: HashSet<(usize, usize)> = HashSet::new();
+    open_set.insert((0, 0));
+    
+    let mut came_from: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+
+    let mut gscore: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut fscore: HashMap<(usize, usize), usize> = HashMap::new();
+    for y in 0..maxy {
+        for x in 0..maxx {
+            gscore.insert((y, x), usize::MAX);
+            fscore.insert((y, x), usize::MAX);
+        }
+    }
+    *gscore.get_mut(&(0, 0)).unwrap() = 0;
+    *fscore.get_mut(&(0, 0)).unwrap() = 0;
+
+    while open_set.len() > 0 {
+        let mut cur = open_set.iter().min_by(|a, b| fscore.get(a).cmp(&fscore.get(b))).unwrap().clone();
+        if cur.0 == maxy - 1 && cur.1 == maxx - 1 {
+            // Arrived at destination, reconstruct path
+            result.push(cur);
+            while came_from.contains_key(&cur) {
+                cur = came_from[&cur];
+                result.insert(0, cur)
+            }
+            return result;
+        }
+
+        open_set.remove(&cur);
+        for n in vec![(0i32, 1i32), (1, 0), (0, -1), (-1, 0)] {
+            // Rust does not make negative offsets easy...
+            let neighbor = match n {
+                (0, 1) => if cur.1 + 1 == maxx { continue } else { (cur.0, cur.1+1) },
+                (1, 0) => if cur.0 + 1 == maxy { continue } else { (cur.0+1, cur.1) },
+                (0, -1) => if cur.1 == 0 { continue } else { (cur.0, cur.1-1) }
+                (-1, 0) => if cur.0 == 0 { continue } else { (cur.0-1, cur.1) },
+                _ => panic!("invalid neighbor")
+            };
+
+            let realy = neighbor.0 % maxmapy;
+            let realx = neighbor.1 % maxmapx;
+            let mapy = neighbor.0 / maxmapy;
+            let mapx = neighbor.1 / maxmapy;
+            let factor = mapy+mapx;
+            let cost = (map[realy][realx] + factor - 1) % 9 + 1;
+
+            let tentative_gscore = gscore[&cur] + cost;
+            if tentative_gscore < gscore[&neighbor] {
+                *came_from.entry(neighbor).or_insert((0, 0)) = cur;
+                *gscore.get_mut(&neighbor).unwrap() = tentative_gscore;
+                *fscore.get_mut(&neighbor).unwrap() = tentative_gscore + (maxy - cur.0) + (maxx - cur.1);
+                if !open_set.contains(&neighbor) {
+                    open_set.insert(neighbor);
+                }
+            }
+        }
+    }
+
+    panic!("no path found")
+}
+
+
 fn parse(lines: &Vec<String>) -> Vec<Vec<usize>> {
     let mut result: Vec<Vec<usize>> = Vec::new();
     for line in lines {
@@ -75,6 +146,15 @@ fn star_one(lines: &Vec<String>) -> usize {
     path.iter().map(|(y, x)| map[*y][*x]).sum::<usize>() - map[0][0]
 }
 
+fn star_two(lines: &Vec<String>) -> usize {
+    let map = parse(lines);
+    let maxy = map.len();
+    let maxx = map[0].len();
+    let path = astartwo(&map);
+
+    path.iter().map(|(y, x)| (map[y % maxy][x % maxx] + (y / maxy) + (x / maxx) - 1) % 9 + 1).sum::<usize>() - map[0][0]
+}
+
 fn main() {
     let file = File::open("./input").expect("Unreadable input file ./input");
     let lines: Vec<String> = io::BufReader::new(file)
@@ -84,6 +164,9 @@ fn main() {
 
     let ans = star_one(&lines);
     println!("Star one: {}", ans);
+
+    let ans = star_two(&lines);
+    println!("Star two: {}", ans);
 }
 
 #[cfg(test)]
@@ -108,5 +191,16 @@ mod tests {
 
         let ans = super::star_one(&lines);
         assert_eq!(ans, 40);
+    }
+
+    #[test]
+    fn test_star_two() {
+        let lines: Vec<String> = TEST_DATA
+            .lines()
+            .map(|x| x.to_string())
+            .collect();
+
+        let ans = super::star_two(&lines);
+        assert_eq!(ans, 315);
     }
 }
