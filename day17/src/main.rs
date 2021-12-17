4eb956ab5b05d01ee30dab8be1ebe61a66d43593
Stijn Hoop <stijn@sandcat.nl>
2021-12-17 13:39:59 +0100

    Day 17 star 1 (ugly!)

---
+++ day17/src/main.rs
@@ -0,0 +1,97 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::vec::Vec;
+
+#[derive(Debug)]
+struct Probe {
+    x: i32,
+    y: i32,
+
+    xvel: i32,
+    yvel: i32,
+    
+    maxy: i32,
+}
+
+#[derive(Debug)]
+struct Target {
+    xmin: i32,
+    xmax: i32,
+    ymin: i32,
+    ymax: i32,
+}
+
+impl Probe {
+    fn new() -> Probe {
+        Probe { x: 0, y: 0, xvel: 0, yvel: 0, maxy: 0 }
+    }
+
+    fn step(&mut self) {
+        self.x += self.xvel;
+        self.y += self.yvel;
+
+        if self.y > self.maxy {
+            self.maxy = self.y;
+        }
+
+        if self.xvel > 0 {
+            self.xvel -= 1;
+        }
+        if self.xvel < 0 {
+            self.xvel += 1;
+        }
+        self.yvel -= 1;
+    }
+
+    fn is_in_target(&self, target: &Target) -> bool {
+        self.x >= target.xmin && self.x <= target.xmax && self.y >= target.ymin && self.y <= target.ymax 
+    }
+
+    fn can_never_reach_target(&self, target: &Target) -> bool {
+        self.y < target.ymin
+    }
+}
+
+fn brute_force(target: &Target) -> i32 {
+    let mut maxy: i32 = 0;
+    for yvel in -200..200 {
+        for xvel in -200..200 {
+            let mut probe = Probe::new();
+            probe.yvel = yvel;
+            probe.xvel = xvel;
+
+            while !probe.is_in_target(target) && !probe.can_never_reach_target(target) {
+                probe.step();
+            }
+
+            if probe.is_in_target(target) {
+                maxy = probe.maxy;
+            }
+        }
+    }
+
+    maxy
+}
+
+
+fn star_one(target: &Target) -> i32 {
+    brute_force(target)
+}
+
+fn main() {
+    // For once just hardcoded the input, parsing does not add value here
+    static STAR_ONE_TARGET: Target = Target { xmin: 153, xmax: 199, ymin: -114, ymax: -75 };
+    let ans = star_one(&STAR_ONE_TARGET);
+    println!("Star one: {}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+    static TEST_DATA: super::Target = super::Target { xmin: 20, xmax: 30, ymin: -10, ymax: -5 };
+
+    #[test]
+    fn test_star_one() {
+        let ans = super::star_one(&TEST_DATA);
+        assert_eq!(ans, 45);
+    }
+}
