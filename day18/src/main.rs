32734f2f9b3c1f2eff143880b764c6478e47b04f
Stijn Hoop <stijn@sandcat.nl>
2021-12-19 12:23:07 +0100

    Day 18 star 1 FINAL

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -2,69 +2,187 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::vec::Vec;
 
-#[derive(Debug, PartialEq, Eq)]
-enum PairType {
-    Pair,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Symbol {
+    LeftParen,
     Number(u32),
+    Comma,
+    RightParen,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Pair {
-    pair_type: PairType,
-    depth: u32,
-    // TODO: convert to Option<Vec<Pair>> as for numbers we don't need this
-    // TODO: alternatively, figure out how to do recursive enum and put the left,right pair into the PairType
-    subpairs: Vec<Pair>,
-}
+fn parse_simple(line: &str) -> Vec<Symbol> {
+    let mut result: Vec<Symbol> = Vec::new();
 
-fn parse_pair(iter: &mut std::str::Chars, depth: u32) -> Pair {
-    let mut result: Pair = Pair { pair_type: PairType::Pair, depth: depth, subpairs: Vec::new() };
-    let ch = iter.next().unwrap();
-    match ch {
-        '[' => {
-            result.subpairs.push(parse_pair(iter, depth + 1));
-        },
-        '0'..='9' => {
-            result.subpairs.push(Pair { pair_type: PairType::Number(ch.to_digit(10).unwrap()), depth: depth, subpairs: Vec::new() });
-        },
-        _ => panic!("Invalid char for left side of pair {}", ch)
+    for ch in line.chars() {
+        result.push(match ch {
+            '[' => Symbol::LeftParen,
+            '0'..='9' => Symbol::Number(ch.to_digit(10).unwrap()),
+            ',' => Symbol::Comma,
+            ']' => Symbol::RightParen,
+            _ => panic!("Invalid char {} in input", ch)
+        });
     }
-    let ch = iter.next().unwrap();
-    if ch != ',' {
-        panic!("Expected comma separator for pair");
-    }
-    let ch = iter.next().unwrap();
-    match ch {
-        '[' => {
-            result.subpairs.push(parse_pair(iter, depth + 1));
-        },
-        '0'..='9' => {
-            result.subpairs.push(Pair { pair_type: PairType::Number(ch.to_digit(10).unwrap()), depth: depth, subpairs: Vec::new() });
-        },
-        _ => panic!("Invalid char for left side of pair {}", ch)
-    }
-    let ch = iter.next().unwrap();
-    if ch != ']' {
-        panic!("Expected right parenthesis to end atom");
-    }
-    
+
     result
 }
 
-fn parse_line(line: &str) -> Pair {
-    let mut iter = line.chars();
-    let ch = iter.next().unwrap();
-    if ch != '[' {
-        panic!("Expected start of pair at start of line");
+fn explode(num: &Vec<Symbol>) -> (Vec<Symbol>, bool) {
+    let mut result: Vec<Symbol> = Vec::new();
+    let mut curdepth: u32 = 0;
+    let mut prev_idx: Option<usize> = None;
+    let mut right_num: Option<u32> = None;
+    let mut iter = num.iter().enumerate();
+    let mut exploded: bool = false;
+    while let Some((idx, sym)) = iter.next() {
+        match sym {
+            Symbol::LeftParen => {
+                curdepth += 1;
+                if curdepth == 5 && !exploded {
+                    // Explode
+                    match iter.next() {
+                        Some((_, Symbol::Number(val))) => {
+                            if !prev_idx.is_none() {
+                                if let Symbol::Number(prev) = result[prev_idx.unwrap()] {
+                                    result[prev_idx.unwrap()] = Symbol::Number(prev + val);
+                                }
+                            }
+                        }
+                        _v => panic!("invalid left side when exploding: {:?}", _v)
+                    }
+                    if let Some((_, Symbol::Comma)) = iter.next() {
+                    } else {
+                        panic!("expected comma when exploding");
+                    }
+                    match iter.next() {
+                        Some((_, Symbol::Number(val))) => {
+                            right_num = Some(*val);
+                        }
+                        _ => panic!("invalid right side when exploding")
+                    }
+                    if let Some((_, Symbol::RightParen)) = iter.next() {
+                    } else {
+                        panic!("expected right parenthesis when exploding");
+                    }
+                    result.push(Symbol::Number(0));
+                    exploded = true;
+                } else {
+                    result.push(*sym);
+                }
+            },
+            Symbol::RightParen => {
+                result.push(*sym);
+                curdepth -= 1;
+            }
+            Symbol::Number(val) => {
+                prev_idx = Some(idx);
+                if !right_num.is_none() {
+                    result.push(Symbol::Number(val + right_num.unwrap()));
+                    right_num = None;
+                } else {
+                    result.push(*sym);
+                }
+            },
+            Symbol::Comma => result.push(*sym),
+        }
     }
 
-    parse_pair(&mut iter, 0)
+    (result, exploded)
 }
 
-fn star_one(_lines: &Vec<String>) -> usize {
-    let _pair = parse_line(&_lines[0]);
+fn split(num: &Vec<Symbol>) -> (Vec<Symbol>, bool) {
+    let mut result: Vec<Symbol> = Vec::new();
+    let mut splitted: bool = false;
 
-    0
+    for sym in num.iter() {
+        match sym {
+            Symbol::Number(val) => {
+                if *val < 10 || splitted {
+                    result.push(*sym);
+                } else {
+                    result.push(Symbol::LeftParen);
+                    result.push(Symbol::Number(*val / 2));
+                    result.push(Symbol::Comma);
+                    result.push(Symbol::Number(val - (*val / 2)));
+                    result.push(Symbol::RightParen);
+                    splitted = true;
+                }
+            },
+            _ => result.push(*sym),
+        }
+    }
+
+    (result, splitted)
+}
+
+fn add(left: &Vec<Symbol>, right: &Vec<Symbol>) -> Vec<Symbol> {
+    let mut result: Vec<Symbol> = Vec::new();
+    result.push(Symbol::LeftParen);
+    result.extend_from_slice(left);
+    result.push(Symbol::Comma);
+    result.extend_from_slice(right);
+    result.push(Symbol::RightParen);
+    result
+}
+
+fn reduce(num: &Vec<Symbol>) -> Vec<Symbol> {
+    let mut result: Vec<Symbol> = num.clone();
+
+    loop
+    {
+        loop
+        {
+            let res = explode(&result);
+            if !res.1 {
+                break;
+            }
+            result = res.0;
+        }
+        let res = split(&result);
+        if !res.1 {
+            break;
+        }
+        result = res.0;
+    }
+
+    result
+}
+
+fn intern_mag(iter: &mut std::slice::Iter<Symbol>) -> u32 {
+    match iter.next() {
+        Some(Symbol::LeftParen) => {},
+        Some(Symbol::Number(val)) => return *val,
+        _ => panic!("Expected number or left parenthesis in magnitude"),
+    }
+
+    let left_mag = intern_mag(iter);
+    if let Some(Symbol::Comma) = iter.next() {
+    } else {
+        panic!("Expected comma in magnitude");
+    }
+    let right_mag = intern_mag(iter);
+    if let Some(Symbol::RightParen) = iter.next() {
+    } else {
+        panic!("Expected right parenthesis in magnitude");
+    }
+
+    left_mag * 3 + right_mag * 2
+}
+
+fn magnitude(num: &Vec<Symbol>) -> u32 {
+    let mut iter = num.iter();
+    intern_mag(&mut iter)
+}
+
+fn star_one(lines: &Vec<String>) -> u32 {
+    let mut iter = lines.iter();
+    let mut num = parse_simple(iter.next().unwrap());
+    while let Some(line) = iter.next() {
+        let right = parse_simple(line);
+        num = add(&num, &right);
+        num = reduce(&num);
+    }
+
+    magnitude(&num)
 }
 
 fn main() {
@@ -80,37 +198,67 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    static PARSE_TEST_DATA: &str = "[1,2]
-[[1,2],3]
-[9,[8,7]]
-[[1,9],[8,5]]
-[[[[1,2],[3,4]],[[5,6],[7,8]]],9]
-[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
-[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]";
+    static TEST_EXPLODE_1: &str = "[[[[[9,8],1],2],3],4]";
 
     #[test]
-    fn parse() {
-        let pair = super::parse_line(PARSE_TEST_DATA.lines().nth(0).unwrap());
-        assert_eq!(pair.pair_type, super::PairType::Pair);
-        assert_eq!(pair.depth, 0);
-        assert_eq!(pair.subpairs[0].pair_type, super::PairType::Number(1));
-        assert_eq!(pair.subpairs[0].depth, 0);
-        assert_eq!(pair.subpairs[1].pair_type, super::PairType::Number(2));
-        assert_eq!(pair.subpairs[1].depth, 0);
-
-        let pair = super::parse_line(PARSE_TEST_DATA.lines().nth(1).unwrap());
-        assert_eq!(pair.pair_type, super::PairType::Pair);
-        assert_eq!(pair.subpairs[0].pair_type, super::PairType::Pair);
-        assert_eq!(pair.subpairs[0].depth, 1);
-        assert_eq!(pair.subpairs[0].subpairs[0].pair_type, super::PairType::Number(1));
-        assert_eq!(pair.subpairs[0].subpairs[0].depth, 1);
-        assert_eq!(pair.subpairs[0].subpairs[1].pair_type, super::PairType::Number(2));
-        assert_eq!(pair.subpairs[0].subpairs[1].depth, 1);
-        assert_eq!(pair.subpairs[1].pair_type, super::PairType::Number(3));
-        assert_eq!(pair.subpairs[1].depth, 0);
+    fn explode_one()
+    {
+        let symbols = super::parse_simple(TEST_EXPLODE_1);
+        let result = super::explode(&symbols);
+        assert_eq!(result.1, true);
+        let symbols = result.0;
+        assert_eq!(symbols[4], super::Symbol::Number(0));
+        assert_eq!(symbols[6], super::Symbol::Number(9));
     }
 
-    static TEST_DATA: &str = "";
+    static TEST_EXPLODE_2: &str = "[[6,[5,[4,[3,2]]]],1]";
+
+    #[test]
+    fn explode_two()
+    {
+        let symbols = super::parse_simple(TEST_EXPLODE_2);
+        let result = super::explode(&symbols);
+        assert_eq!(result.1, true);
+        let symbols = result.0;
+        assert_eq!(symbols[8], super::Symbol::Number(7));
+        assert_eq!(symbols[10], super::Symbol::Number(0));
+        assert_eq!(symbols[15], super::Symbol::Number(3));
+    }
+
+    static TEST_REDUCE: &str = "[[[[4,3],4],4],[7,[[8,4],9]]]
+[1,1]";
+
+    #[test]
+    fn reduce()
+    {
+        let left = super::parse_simple(TEST_REDUCE.lines().nth(0).unwrap());
+        let right = super::parse_simple(TEST_REDUCE.lines().nth(1).unwrap());
+        let num = super::add(&left, &right);
+        let red = super::reduce(&num);
+        assert_eq!(red[4], super::Symbol::Number(0));
+        assert_eq!(red[14], super::Symbol::Number(7));
+    }
+
+    static TEST_MAGNITUDE: &str = "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]";
+
+    #[test]
+    fn magnitude()
+    {
+        let num = super::parse_simple(TEST_MAGNITUDE);
+        let mag = super::magnitude(&num);
+        assert_eq!(mag, 3488);
+    }
+
+    static TEST_DATA: &str = "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
 
     #[test]
     fn test_star_one() {
@@ -120,6 +268,6 @@ mod tests {
             .collect();
 
         let ans = super::star_one(&lines);
-        assert_eq!(ans, 100);
+        assert_eq!(ans, 4140);
     }
 }
